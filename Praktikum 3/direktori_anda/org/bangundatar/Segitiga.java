8ed769ad0be37d2fcf23aa5f3aeb0d7345f30d46
nikosinabariba01 <100134100+nikosinabariba01@users.noreply.github.com>
2023-03-12 22:50:19 +0700

    Add files via upload

---
+++ Praktikum 3/direktori_anda/org/bangundatar/Segitiga.java
@@ -0,0 +1,27 @@
+/**
+ * File : PersegiPanjang.java 08/03/2023
+ * Penulis : Niko Geraldi Sinabariba
+ * Deskripsi : representasi dari objek persegi panjang, turunan kelas poligon
+ */
+ 
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon{
+	private double alas, tinggi;
+	
+	public Segitiga(double alas,double tinggi, int jumlahSisi){
+		this.alas = alas;
+		this.tinggi = tinggi;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas(){
+		return 0.5*alas*tinggi;
+	}
+	
+	public void printInfo(){
+		System.out.println("\nBangun Segitiga bersisi "+ this.getJumlahSisi());
+	}
+}
