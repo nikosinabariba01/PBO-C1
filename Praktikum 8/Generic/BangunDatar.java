1a86e3a27738447101c0ff16635e8c5f81c3e4e2
nikosinabariba01 <100134100+nikosinabariba01@users.noreply.github.com>
2023-05-25 16:47:28 +0700

    Add files via upload

---
+++ Praktikum 8/Generic/BangunDatar.java
@@ -0,0 +1,9 @@
+/*
+ * File : BangunDatar.java  
+ * Penulis : Niko Geraldi Sinabariba (24060121140126)
+ * Deskripsi : kelas abstrak untuk bangun datar
+*/
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
