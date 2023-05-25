42da72c6be6eb132ba2859537a92c7ab6700f3be
nikosinabariba01 <100134100+nikosinabariba01@users.noreply.github.com>
2023-05-25 16:43:04 +0700

    Delete Praktikum 8/Prak 8 directory

--- Praktikum 8/Prak 8/Generic/BangunDatar.java
+++
@@ -1,9 +0,0 @@
-/*
- * File : BangunDatar.java  
- * Penulis : Niko Geraldi Sinabariba (24060121140126)
- * Deskripsi : kelas abstrak untuk bangun datar
-*/
-
-public abstract class BangunDatar {
-    public abstract double hitungKeliling();
-}
