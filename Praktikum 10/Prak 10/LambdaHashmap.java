d95a2b1473adf4f2f34e47a26467681efdbc06c2
nikosinabariba01 <100134100+nikosinabariba01@users.noreply.github.com>
2023-06-06 22:04:09 +0700

    Add files via upload

---
+++ Praktikum 10/Prak 10/LambdaHashmap.java
@@ -0,0 +1,20 @@
+/* 
+ * File : LambdaHashmap.java  (06/06/2023)
+ * Penulis : Niko Geraldi Sinabariba (24060121140150) 
+ * Deskripsi: Menggunakan lambda untuk menampilkan data mahasiswa
+*/
+
+import java.util.HashMap;
+
+public class LambdaHashmap {
+    public static void main(String[] args) {
+        HashMap<String, String> mahasiswaMap = new HashMap<>();
+        mahasiswaMap.put("24060121140126", "Niko Geraldi Sinabariba");
+        mahasiswaMap.put("24060121130021", "joo");
+        mahasiswaMap.put("24060121140565", "wawa");
+        mahasiswaMap.put("24060121140781", "lee");
+
+        // menggunakan lambda
+        mahasiswaMap.forEach((nim, nama) -> System.out.println(nim + " : " + nama));
+    }
+}
